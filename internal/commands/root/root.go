d9b280f95f740449e4cdaf6efb2ac1378ad27439
Pires <pjpires@gmail.com>
2021-01-12 12:59:23 +0000

    wip
    Signed-off-by: Pires <pjpires@gmail.com>

--- internal/commands/root/root.go
+++ internal/commands/root/root.go
@@ -35,7 +35,7 @@ import (
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/kubernetes/typed/coordination/v1beta1"
+	coordv1 "k8s.io/client-go/kubernetes/typed/coordination/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -166,9 +166,9 @@ func runRootCommandWithProviderAndClient(ctx context.Context, pInit provider.Ini
 		"watchedNamespace": c.KubeNamespace,
 	}))
 
-	var leaseClient v1beta1.LeaseInterface
+	var leaseClient coordv1.LeaseInterface
 	if c.EnableNodeLease {
-		leaseClient = client.CoordinationV1beta1().Leases(corev1.NamespaceNodeLease)
+		leaseClient = client.CoordinationV1().Leases(corev1.NamespaceNodeLease)
 	}
 
 	nodeProvider, ok := p.(node.NodeProvider)
@@ -180,7 +180,7 @@ func runRootCommandWithProviderAndClient(ctx context.Context, pInit provider.Ini
 		nodeProvider,
 		pNode,
 		client.CoreV1().Nodes(),
-		node.WithNodeEnableLeaseV1Beta1(leaseClient, nil),
+		node.WithNodeEnableLeaseV1(leaseClient, 0),
 		node.WithNodeStatusUpdateErrorHandler(func(ctx context.Context, err error) error {
 			if !k8serrors.IsNotFound(err) {
 				return err
